ab12cd22b1addabb6e4af94ffa2b2baf329a49ac
silver0-stack <dev.choiey@gmail.com>
2023-03-28 16:16:15 +0900

    인증 백엔드 통합 파일 추가

---
+++ src/main/java/com/example/demo/contorller/UserController.java
@@ -0,0 +1,4 @@
+package com.example.demo.contorller;
+
+public class UserController {
+}
---
+++ src/main/java/com/example/demo/model/UserEntity.java
@@ -0,0 +1,31 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = "email")})
+public class UserEntity {
+    @Id
+    @GeneratedValue(generator ="system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
+    private String id; // 사용자에게 고유하게 부여되는 id
+
+    @Column(nullable = false)
+    private String  username; // 사용자의 이름
+
+    @Column(nullable = false)
+    private String email; // 사용자의 이메일, 아이디와 같은 기능을 한다
+
+    @Column(nullable = false)
+    private String password; //패스워드
+}
+
