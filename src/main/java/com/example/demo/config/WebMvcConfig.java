c4f4555a15aa2a14aec15def25b776d15f23c7fd
silver0-stack <dev.choiey@gmail.com>
2023-04-05 20:57:29 +0900

    CORS - 프론트엔드 주소 추가

--- src/main/java/com/example/demo/config/WebMvcConfig.java
+++ src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -11,7 +11,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000",
+                        "http://todoapplication-frontend-dev2.us-west-2.elasticbeanstalk.com")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
